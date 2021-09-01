2b0cb63e048c1b5d1feef1f4a637128ed7fa1762
Andrii.Mekhanich <89304608+MekhAnd@users.noreply.github.com>
2021-09-01 11:54:30 +0300

    Update Main.java

--- HomeWork1c/src/com/gmail/mekhanich/andrii/Main.java
+++ HomeWork1c/src/com/gmail/mekhanich/andrii/Main.java
@@ -9,12 +9,9 @@ public class Main {
 		Phone three = new Phone(100003, "John Gray");
 		Phone four = new Phone(100004, "Green Gray");
 		Phone five = new Phone(100005, "Green Gray");
-<<<<<<< HEAD
-		Phone six = new Phone(100006, "One, One");
+		Phone six = new Phone(100006, "One, Two");
 		Phone myPhone= new Phone(100008, "One, One");
-=======
-		Phone six = new Phone(100006, "One One");
->>>>>>> 8b49bbf41a6e1140ad2517e3649160fd7dfceef9
+		
 		
 		Network firstMobyle = new Network("FM");
 		
